07fc5d01a1d18530436da71720f69f5e70ec8330
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-03 19:39:39 +0200

    fix tests

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var test = require('ava');
 var untildify = require('./');
 
 test(function (t) {
-	process.env.HOME = '/Users/sindresorhus';
-	t.assert(untildify('~/dev') === '/Users/sindresorhus/dev');
+	var fixture = '~/dev';
+	t.assert(/\/dev/.test(untildify(fixture)));
+	t.assert(untildify(fixture) !== fixture);
 });
