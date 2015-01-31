f997f034b1a028448b266b617f6cb4c3b0269d0b
silverwind <silv3rwind@gmail.com>
2015-01-31 21:01:39 +0100

    Throw if input is not a string

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 var userHome = require('user-home');
 
 module.exports = function (str) {
+	if (typeof str !== 'string') {
+		throw new TypeError('Expected a string');
+	}
 	return userHome ? str.replace(/^~\//, userHome + '/') : str;
 };
