173ed6e27908f7b83aac05c29b5434e4121c807b
silverwind <me@silverwind.io>
2015-02-01 21:01:14 +0100

    Split up tests and add some more

--- test.js
+++ test.js
@@ -6,10 +6,44 @@ test(function (t) {
 	var fixture = '~/dev';
 	t.assert(/\/dev/.test(untildify(fixture)));
 	t.assert(untildify(fixture) !== fixture);
+});
+
+test(function (t) {
 	t.assert(!/^~$/.test(untildify('~')));
+});
+
+test(function (t) {
 	t.assert(/^~abc$/.test(untildify('~abc')));
+});
+
+test(function (t) {
 	t.assert(/.+\/$/.test(untildify('~/')));
+});
+
+test(function (t) {
 	t.assert(/.+\\$/.test(untildify('~\\')));
+});
+
+test(function (t) {
 	t.assert(/.+\/abc$/.test(untildify('~/abc')));
+});
+
+test(function (t) {
 	t.assert(/.+\\abc$/.test(untildify('~\\abc')));
+});
+
+test(function (t) {
+	t.assert(/.+\/abc\/def$/.test(untildify('~/abc/def')));
+});
+
+test(function (t) {
+	t.assert(/.+\\abc\\def$/.test(untildify('~\\abc\\def')));
+});
+
+test(function (t) {
+	t.assert(/.+\/abc\\def$/.test(untildify('~/abc\\def')));
+});
+
+test(function (t) {
+	t.assert(/.+\\abc\/def$/.test(untildify('~\\abc/def')));
 });
