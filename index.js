82f6aa57dd2c615d561960d3a5f006b8badb6236
James Arlow <james.arlow@gmail.com>
2016-12-22 14:12:55 -0500

    Simplified regex to support passthrough $vars
    Addressing: [Issue 5](https://github.com/sindresorhus/untildify/issues/5)
    
    The original regex attempted to capture and reuse the path delimiter after the tilde.
    
    This broke the substitution when a regex variable was used as the base of the path.
    
    works:
    * `~/`
    * `~/$1`
    
    did not work:
    
    * `~$1`
    
    To fix, I simply replaced only the leading tilde.  
    This is probably more performant (simpler regex, same delimiter reuse), and opens up more use cases.
    
    Note: this will not solve [Issue 1](https://github.com/sindresorhus/untildify/issues/1), 
    but should not conflict with a fix more than the original implementation.
    the original implementation.

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ module.exports = str => {
 		throw new TypeError(`Expected a string, got ${typeof str}`);
 	}
 
-	return home ? str.replace(/^~($|\/|\\)/, `${home}$1`) : str;
+	return home ? str.replace(/^~/, `${home}`) : str;
 };
