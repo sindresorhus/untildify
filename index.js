b204c37134d3e133caa9ba8db8d50401c1cce1af
james <james.arlow@gmail.com>
2016-12-22 15:43:43 -0500

    Test cases and guard against explicit `~username`.

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ module.exports = str => {
 		throw new TypeError(`Expected a string, got ${typeof str}`);
 	}
 
-	return home ? str.replace(/^~/, `${home}`) : str;
+	/* Only replace if followed by path delimiter or regex variable. */
+	if (/^~[^$$|\\|/]/.test(str)) {
+		return str;
+	}
+
+	/* If valid swap tilde. */
+	return str.replace(/^~/, `${home}`);
 };
