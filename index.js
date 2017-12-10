458664a855de4d5394593e7cc55c1e6175448cd8
Mathias Bynens <mathias@qiwi.be>
2017-12-10 02:01:51 +0100

    Avoid issues with non-ASCII symbols on Windows
    On Windows, we want to get the home directory with the user name part in 8.3 format (https://goo.gl/FxW2CB) if needed. This is a workaround for https://github.com/nodejs/node/issues/17586.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 'use strict';
 const os = require('os');
-const home = os.homedir();
+const isWindows = require('is-windows');
+
+const home = (() => {
+	const homeDir = os.homedir();
+	if (isWindows()) {
+		// Return the home directory with the user name part in 8.3 format
+		// (https://goo.gl/FxW2CB) if needed. This is a workaround for
+		// https://github.com/nodejs/node/issues/17586.
+		const tmpDir = os.tmpdir();
+		const userDir = homeDir.replace(/([^\\])+$/g, '');
+		const user83 = tmpDir.slice(userDir.length).replace(/\\.*$/, '');
+		return `${userDir}${user83}`;
+	}
+	return homeDir;
+})();
 
 module.exports = str => {
 	if (typeof str !== 'string') {
