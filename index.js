ea4ce867daa9bb1970465474cc5fc0b5674fafa1
Raphael von der Grün <raphinesse@gmail.com>
2018-05-16 14:58:21 +0200

    Use lookahead expression for cleaner replace
    This is just code golfing. But I like it better this way.

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ module.exports = str => {
 		throw new TypeError(`Expected a string, got ${typeof str}`);
 	}
 
-	return home ? str.replace(/^~($|\/|\\)/, `${home}$1`) : str;
+	return home ? str.replace(/^~(?=$|\/|\\)/, home) : str;
 };
