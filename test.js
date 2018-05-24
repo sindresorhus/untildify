7c86e40ca5ea09e6aa9136c691d1f42fcb328089
Raphael von der Grün <raphinesse@gmail.com>
2018-05-24 19:56:23 +0200

    Harden tests (#16)
    This extends and hardens the existing test suite.
    The tests added in 853d88a show that #5 is not actually a problem.

--- test.js
+++ test.js
@@ -1,17 +1,48 @@
 import test from 'ava';
-import m from '.';
+import rewire from 'rewire';
 
-test(t => {
-	t.true(m('~/dev') !== '~/dev');
-	t.true(/\/dev/.test(m('~/dev')));
-	t.true(!/^~$/.test(m('~')));
-	t.true(/^~abc$/.test(m('~abc')));
-	t.true(/.+\/$/.test(m('~/')));
-	t.true(/.+\\$/.test(m('~\\')));
-	t.true(/.+\/abc$/.test(m('~/abc')));
-	t.true(/.+\\abc$/.test(m('~\\abc')));
-	t.true(/.+\/abc\/def$/.test(m('~/abc/def')));
-	t.true(/.+\\abc\\def$/.test(m('~\\abc\\def')));
-	t.true(/.+\/abc\\def$/.test(m('~/abc\\def')));
-	t.true(/.+\\abc\/def$/.test(m('~\\abc/def')));
+const m = rewire('.');
+const MOCK_HOME = 'MOCK_HOME';
+
+const expandsTildePrefixWithHome = tildePrefixedString => m(tildePrefixedString) === MOCK_HOME + tildePrefixedString.slice(1);
+
+// Mock out the home directory for more accurate and explicit tests
+// eslint-disable-next-line ava/use-t
+test.beforeEach(_ => m.__set__('home', MOCK_HOME));
+
+test('operation with home directory', t => {
+	t.not(m('~'), '~');
+	t.not(m('~/dev'), '~/dev');
+	t.regex(m('~/dev'), /\/dev/);
+	t.true(expandsTildePrefixWithHome('~/'));
+	t.true(expandsTildePrefixWithHome('~\\'));
+	t.true(expandsTildePrefixWithHome('~/abc'));
+	t.true(expandsTildePrefixWithHome('~\\abc'));
+	t.true(expandsTildePrefixWithHome('~/abc/def'));
+	t.true(expandsTildePrefixWithHome('~\\abc\\def'));
+	t.true(expandsTildePrefixWithHome('~/abc\\def'));
+	t.true(expandsTildePrefixWithHome('~\\abc/def'));
+});
+
+test('operation without home directory', t => {
+	m.__set__('home', undefined);
+	t.is(m('~'), '~');
+	t.is(m('foo'), 'foo');
+	t.is(m('~abc'), '~abc');
+	t.is(m('~/dev'), '~/dev');
+});
+
+test('paths where ~ is not current user\'s home directory', t => {
+	t.is(m('~abc'), '~abc');
+	t.is(m('/~/'), '/~/');
+	t.is(m('/~'), '/~');
+	t.is(m('abc~'), 'abc~');
+	t.true(expandsTildePrefixWithHome('~/abc~'));
+});
+
+test('paths with regex replacement patterns', t => {
+	t.is(m('~$&'), '~$&');
+	t.is(m('~$1'), '~$1');
+	t.true(expandsTildePrefixWithHome('~/$1'));
+	t.true(expandsTildePrefixWithHome('~/$&'));
 });
